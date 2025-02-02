ac58321485bc7a42744b3badb5072480dd7804a8
Jove Jonovski <jovejonovski@gmail.com>
2025-02-02 01:09:16 +0100

    Logo improvements

--- src/components/Buttons/Logo.vue
+++ src/components/Buttons/Logo.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { TooltipProps } from "naive-ui";
 import { NPerformantEllipsis } from "naive-ui";
 import { useContextStore } from "~/stores/context.ts";
 import RouterButton from "./RouterButton.vue";
@@ -9,6 +10,12 @@ defineOptions({
 });
 
 const context = useContextStore();
+
+const tooltipProps: TooltipProps = {
+  width: 280,
+  delay: 0,
+  placement: "bottom-start",
+};
 </script>
 
 <template>
@@ -17,7 +24,7 @@ const context = useContextStore();
       <img :src="context.logo" class="h-18px w-18px border-none" loading="lazy">
     </template>
 
-    <NPerformantEllipsis class="max-w-150px line-height-relaxed">
+    <NPerformantEllipsis class="text-initial font-500 line-height-relaxed !max-w-200px" :tooltip="tooltipProps">
       {{ context.siteName }}
     </NPerformantEllipsis>
   </RouterButton>
